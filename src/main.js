ce633f19a4ead5cba7454aca4e252063f1c4e113
denis <deadkor26@gmail.com>
2024-02-05 00:16:51 +0200

    new  loader

--- src/main.js
+++ src/main.js
@@ -1,105 +1,111 @@
-// ----library iziToast----
+// Підключення бібліотек
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-import closeIcon from './img/bi_x-octagon.png';
-
-// ----library simpleLigthbox----
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import closeIcon from './img/bi_x-octagon.png';
 
+// Отримання посилань на необхідні DOM-елементи
 const form = document.querySelector('form');
 const list = document.querySelector('.gallery');
+
+// Додаємо обробник події для кнопки пошуку
 form.addEventListener('submit', onSearchButton);
 
-// ----Event Searching photos----
-function onSearchButton(e){
+// Функція обробки події пошуку
+function onSearchButton(e) {
     e.preventDefault();
-    const inputSearch = form.elements.search.value;
+    const inputSearch = form.elements.search.value.trim();
     if (inputSearch === '') {
-        iziToast.error({
-        messageColor: '#FFF',
-        color: '#EF4040',
-        iconUrl: closeIcon,
-        position: 'topRight',
-        message: 'Please,enter what do you want to find!',
-        });
+        showErrorMessage('Please, enter what you want to find!');
         return;
     }
-    form.insertAdjacentHTML('afterend', '<span class="loader"></span>');
+    addClass(form, 'loading');
     getPhotos(inputSearch);
     form.reset();
 }
 
-// ----Promise function----
+// Додати клас до елементу
+function addClass(element, className) {
+    element.classList.add(className);
+}
+
+// Отримання фотографій за допомогою API
 function getPhotos(inputSearch) {
     const searchParams = new URLSearchParams({
-    key: "42112521-3ff4dfc201bab0977369cd2bc",
-    q: `${inputSearch}`,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
+        key: "31960717-8c82352157f13e8057b3011e3",
+        q: inputSearch,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true",
     });
-     
-const url = `https://pixabay.com/api/?${searchParams}`;
+
+    const url = `https://pixabay.com/api/?${searchParams}`;
     return fetch(url)
-        .then(response => { return response.json() })
+        .then(response => response.json())
         .then(photos => {
             const arrayPhotos = photos.hits;
-            if (arrayPhotos.length === 0) { noImages() };
-            const spanLoader = document.querySelector('.loader');
+            if (arrayPhotos.length === 0) {
+                noImages();
+            }
+            removeClass(form, 'loading');
             renderPhoto(arrayPhotos);
-            spanLoader.remove();
         })
         .catch(error => {
-            iziToast.error({
-                messageColor: '#FFF',
-                color: '#EF4040',
-                iconUrl: closeIcon,
-                position: 'topRight',
-                message: `${error}`,
-            })
+            showErrorMessage(error);
+            removeClass(form, 'loading');
         });
 }
 
-// ----When photos are not found----
+// Видалити клас з елементу
+function removeClass(element, className) {
+    element.classList.remove(className);
+}
+
+// Обробник, який викликається, коли фотографії не знайдені
 function noImages() {
-    list.innerHTML='';
+    list.innerHTML = '';
+    showErrorMessage('Sorry, there are no images matching your search query. Please try again!');
+}
+
+// Рендеринг фотографій
+function renderPhoto(photos) {
+    const markup = photos
+        .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) =>
+            `<li class='gallery-item'>
+                <a class='gallery-link' href='${largeImageURL}'>
+                    <img class='gallery-image' src='${webformatURL}' alt='${tags}'/>
+                </a>
+                <div class='container-app'>
+                    <p><span>Likes</span> ${likes}</p>
+                    <p><span>Views</span> ${views}</p>
+                    <p><span>Comments</span> ${comments}</p>
+                    <p><span>Downloads</span> ${downloads}</p>
+                </div>
+            </li>`)
+        .join('');
+    list.insertAdjacentHTML('afterBegin', markup);
+    initLightbox();
+}
+
+// Ініціалізація Lightbox
+function initLightbox() {
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionsPosition: 'bottom',
+        captionDelay: 250,
+    });
+    gallery.on('show.simpleLightbox');
+    gallery.refresh();
+}
+
+// Відображення повідомлення про помилку
+function showErrorMessage(message) {
     iziToast.error({
         messageColor: '#FFF',
         color: '#EF4040',
         iconUrl: closeIcon,
         position: 'topRight',
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-        });
-}
-
-// ----Markup HTML----
-function renderPhoto(photos) {
-    const markup = photos
-        .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) =>
- `<li class='gallery-item'>
-  <a class='gallery-link' href='${largeImageURL}'>
-    <img class='gallery-image' src='${webformatURL}' alt='${tags}'/>
-  </a>
-<div class='container-app'>
-<p><span>Likes</span> ${likes}</p>
-<p><span>Views</span> ${views }</p>
-<p><span>Comments</span> ${comments}</p>
-<p><span>Downloads</span> ${downloads}</p>
-</div>
- </li>`)
-        .join('');
-    list.insertAdjacentHTML('afterBegin', markup);
-    simpleLightbox();
-}
-
-// ----library simpleLightbox----
-function simpleLightbox(){
-    let gallery = new SimpleLightbox('.gallery a',{
-    captionsData: 'alt',
-    captionsPosition: 'bottom',
-    captionDelay: 250,
-});
-    gallery.on('show.simpleLightbox');
-    gallery.refresh();
+        message: message,
+    });
 }
